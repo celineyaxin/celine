3e32d7a720393f74fd75643ae58df61903d0fbad
chenyaxin <cyx17826850516@126.com>
2025-04-03 11:09:47 +0800

    adjust

--- consumer_complaint_data/replace_xiaoyi.py
+++ consumer_complaint_data/replace_xiaoyi.py
@@ -1,16 +1,15 @@
 ##complaints_part_3已经完成了文件分类需要替换分期易
 
 import pandas as pd
-file2_path = '/Users/chenyaxin/Desktop/websitdata/merge_data4/分期易.csv'  
+file2_path = '/Users/chenyaxin/Desktop/催收/分期易.csv'  
 df2 = pd.read_csv(file2_path)
 
 code_and_business_dict = dict(zip(df2['投诉编号'], 
                                    list(zip(df2['编码'], df2['投诉商家']))))
 
-file1_path = '/Users/chenyaxin/Desktop/websitdata/similarity/complaints_part_3.csv'  
-# file1_path = '/Users/chenyaxin/Desktop/websitdata/test.csv'  
-
+file1_path = '/Users/chenyaxin/Desktop/催收/delete_hostility.csv'  
 df1 = pd.read_csv(file1_path)
+original_complainant_count_for_lv = df1[df1['投诉商家'] == '分期乐'].shape[0]
 
 # 遍历文件1中的每一行，并更新编码和投诉商家
 for index, row in df1.iterrows():
@@ -19,7 +18,13 @@ for index, row in df1.iterrows():
         df1.at[index, '编码'] = code
         df1.at[index, '投诉商家'] = business
 
+complainant_count_for_lv_after = df1[df1['投诉商家'] == '分期乐'].shape[0]
+complainant_count_for_eyi_after = df1[df1['投诉商家'] == '分期易'].shape[0]
 
-output_path = '/Users/chenyaxin/Desktop/websitdata/similarity/updated_complaints_part_3.csv'  # 输出文件路径
+output_path = '/Users/chenyaxin/Desktop/催收/financial_final.csv'  # 输出文件路径
 df1.to_csv(output_path, index=False)
 print(f"更新完成，结果已保存到 '{output_path}'")
+
+print(f"替换前投诉商家为分期乐的投诉数量：{original_complainant_count_for_lv}")
+print(f"替换后投诉商家为分期乐的投诉数量：{complainant_count_for_lv_after}")
+print(f"替换后投诉商家为分期易的投诉数量：{complainant_count_for_eyi_after}")
--- readme.md
+++ readme.md
@@ -1,7 +1,27 @@
 项目名称
-# consumer_complaint_data
-提取金融类商家投诉文本数据用于实证分析
-# local_station_data
-获得不同地区的文本内容
-# supply_chain_risk
-构造指标
+# 1. consumer_complaint_data
+* 爬取黑猫数据建立投诉数据库
+* 提取金融类商家投诉文本数据用于实证分析
+
+# 2. local_station_data
+* 根据金融类的投诉文本获得不同地区的信息
+
+# 3. supply_chain_risk
+* 通过年报文本数据构造供应链风险指标
+
+# 4. travel
+* 提取互联网旅游类经营的商家
+* 对投诉文本做分类
+
+# 5. ai_publicfirm
+* 根据提取出来的上市公司投诉数据进行探索
+
+# 6. articles_of_association
+* 爬取巨潮网章程数据并分条写入
+
+# 7. debt_collection
+* 获取催收的投诉内容
+
+# others
+* 非具体工程
+
